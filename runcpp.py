a6bbce6ca8dc15e1fc39cbc93ea2d0092a63bc42
Karanveer Plaha <7011511+karanveersp@users.noreply.github.com>
2018-03-24 09:25:43 -0400

    Added the runcpp.py file
    Upload of the first iteration of the runcpp.py script.

---
+++ runcpp.py
@@ -0,0 +1,40 @@
+# Script that compiles and executes a .cpp file
+# Usage:
+# python runcpp.py -i <filename> (without .cpp extension)
+
+import getopt, subprocess, sys
+
+def main(argv):
+    cpp_file = ''
+    exe_file = ''
+    try:
+        opts, args = getopt.getopt(argv, "hi:",["help",'ifile=']) # This parses the arguments into lists
+    except getopt.GetoptError as err:
+        # print help information and exit
+        print(err)      
+        usage()
+        sys.exit(2)
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif o in ("-i", "--ifile"):
+            cpp_file = a + '.cpp'
+            exe_file = a + '.exe'
+            run(cpp_file, exe_file)
+            
+
+def usage():
+    print('runcpp.py -i <filename> (without .cpp extension)')
+
+def run(cpp_file, exe_file):
+    x = subprocess.getoutput('g++ ' + cpp_file + ' -o ' + exe_file)     # Compile the .cpp file and store any error message
+    if x == "": # no error message
+        subprocess.run(exe_file)        # run the program
+    else:
+        print(x)                        # display the error
+
+if __name__=='__main__':
+    main(sys.argv[1:])
+
+
